35c8bb444ea7331ea474b558ea1a3d7c6ae5f442
suba <84484832+suba327777@users.noreply.github.com>
2024-07-04 16:53:28 +0900

    nvim:refactor

--- nvim/after/plugin/null-ls.rc.lua
+++ nvim/after/plugin/null-ls.rc.lua
@@ -8,9 +8,9 @@ null_ls.setup({
     }),
     null_ls.builtins.formatting.prettier,
 
-    -- null_ls.builtins.diagnostics.eslint_d.with({
-    --   diagnostics_format = '[eslint] #{m}\n(#{c})'
-    -- }),
-    -- null_ls.builtins.diagnostics.zsh
+    null_ls.builtins.diagnostics.eslint_d.with({
+      diagnostics_format = '[eslint] #{m}\n(#{c})'
+    }),
+    null_ls.builtins.diagnostics.zsh
   }
 })
