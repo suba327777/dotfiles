6161d00ece59662524583e1d6e09c621ff3c6092
suba327777 <subachav@gmail.com>
2026-04-18 19:32:45 +0900

    nvim: update color scheme nightfox

---
+++ nvim/after/plugin/nightfox.rc.lua
@@ -0,0 +1,86 @@
+vim.opt.termguicolors = true
+
+require("nightfox").setup({
+  options = {
+    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+    compile_file_suffix = "_compiled",
+
+    transparent = true,
+
+    terminal_colors = true,
+    dim_inactive = false,
+    module_default = true,
+
+    colorblind = {
+      enable = false,
+      simulate_only = false,
+      severity = {
+        protan = 0,
+        deutan = 0,
+        tritan = 0,
+      },
+    },
+
+    styles = {
+      comments = "NONE",
+      conditionals = "NONE",
+      constants = "NONE",
+      functions = "NONE",
+      keywords = "NONE",
+      numbers = "NONE",
+      operators = "NONE",
+      strings = "NONE",
+      types = "NONE",
+      variables = "NONE",
+    },
+
+    inverse = {
+      match_paren = false,
+      visual = false,
+      search = false,
+    },
+
+    modules = {},
+  },
+
+  palettes = {},
+  specs = {},
+
+  groups = {
+    all = {
+      -- メイン背景透過
+      Normal = { bg = "NONE" },
+      NormalNC = { bg = "NONE" },
+
+      -- サイド列
+      SignColumn = { bg = "NONE" },
+      LineNr = { bg = "NONE" },
+      CursorLineNr = { bg = "NONE" },
+      EndOfBuffer = { bg = "NONE" },
+
+      -- 浮遊ウィンドウ
+      NormalFloat = { bg = "NONE" },
+      FloatBorder = { bg = "NONE" },
+
+      -- 分割線
+      VertSplit = { bg = "NONE" },
+      WinSeparator = { bg = "NONE" },
+
+      -- Telescope
+      TelescopeNormal = { bg = "NONE" },
+      TelescopeBorder = { bg = "NONE" },
+
+      -- WhichKey / popup系
+      Pmenu = { bg = "NONE" },
+      PmenuSel = { bg = "#3b4261" },
+
+      -- 診断表示
+      DiagnosticVirtualTextError = { bg = "NONE" },
+      DiagnosticVirtualTextWarn = { bg = "NONE" },
+      DiagnosticVirtualTextInfo = { bg = "NONE" },
+      DiagnosticVirtualTextHint = { bg = "NONE" },
+    },
+  },
+})
+
+vim.cmd("colorscheme nightfox")
